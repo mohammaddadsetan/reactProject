34076c9d7aa973edabbd463ae8f6839115b9b2b7
Mohammad <dadsetanpor@gmail.com>
2024-09-09 23:50:21 +0330

    complete search box

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import arrowimg from "./img/arrowimg.png";
 import logo from "./img/logo.png";
 import ReactDOM from 'react-dom/client';
 import bannerpic from './img/michelangelodavid.jpg';
+import searchIcon from './img/search-icon.png'
 import './index.css';
 // import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -19,7 +20,7 @@ function Wbutton(props) {
 
 function Button(props) {
   return (
-    <div  className='a-button'><a href="#">{props.buttonName.bname}</a></div>
+    <div className='a-button'><a href="#">{props.buttonName.bname}</a></div>
   )
 }
 
@@ -35,32 +36,44 @@ root.render(
     <header>
       <div className='header-logo'><img src={logo} alt="" /></div>
       <div className='header-options'>
-      <Arrowbutton buttonName={{bname:'PRODUCTS'}}/>
-      <Button buttonName={{bname:'TEMPLATES'}}/>
-      <Arrowbutton buttonName={{bname:'RESORCES'}}/>
+        <Arrowbutton buttonName={{ bname: 'PRODUCTS' }} />
+        <Button buttonName={{ bname: 'TEMPLATES' }} />
+        <Arrowbutton buttonName={{ bname: 'RESORCES' }} />
       </div>
       <div className='header-login'>
-        <Button buttonName={{bname:'LOGIN'}}/>
-        <Wbutton buttonName={{bname:'SHOP'}}/>
+        <Button buttonName={{ bname: 'LOGIN' }} />
+        <Wbutton buttonName={{ bname: 'SHOP' }} />
       </div>
     </header>
-<main>
-<section className='banner'>
-<img src={bannerpic} alt="" />
-<div className='banner-text'>
-<h1>Designed to sell</h1>
-<div className='banner-subtext'>
-<Wbutton buttonName={{bname:'SHOP'}}/>
-<p>Get your free website trial today.
-No credit card required.</p>
-</div>
+    <main>
+      <section className='banner'>
+        <img src={bannerpic} alt="" />
+        <div className='banner-text'>
+          <h1>Designed to sell</h1>
+          <div className='banner-subtext'>
+            <Wbutton buttonName={{ bname: 'SHOP' }} />
+            <p>Get your free website trial today.
+              No credit card required.</p>
+          </div>
+        </div>
+      </section>
+      <section className='search padding'>
+        <div className='search-text'>
+          <span>Find a domain name for your website</span>
+        </div>
+        <div className='search-container'>
+          <div className='search-box'>
+            <img src={searchIcon} alt="" />
+            <input type="text" placeholder='search for a domain' />
+            <button className='search-button'>SEARCH</button>
+          </div>
+          <a href="">Learn more about Domains</a>
+        </div>
+      </section>
 
-</div>
 
 
-</section>
-
-</main>
+    </main>
 
 
 
